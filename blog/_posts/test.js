9dd731d7d5c1826467e93fe6e0af1710e9203fd3
paddingme <huhaibo2012@gmail.com>
2014-04-25 01:10:02 +0800

    new paper

--- blog/_posts/test.js
+++ blog/_posts/test.js
@@ -1,9 +1,12 @@
-function greeting(name){
-	var text='Hello'+name;
-	return function() {
-		alert(text);
-	}
+var authorName="山边小溪";
+function doSomething(){
+    var blogName="梦想天空";
+    function innerSay(){
+        alert(blogName);
+    }
+    innerSay();
 }
-
-var sayHello=greeting("Closure");
-sayHello();
+alert(authorName); //山边小溪
+alert(blogName); //脚本错误
+doSomething(); //梦想天空
+innerSay() //脚本错误
