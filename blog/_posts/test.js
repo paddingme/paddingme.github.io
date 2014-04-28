9d49dff2e1c380f9ef1ba1505ae77e8e79a70121
paddingme <huhaibo2012@gmail.com>
2014-04-28 21:24:22 +0800

    new blogs

--- blog/_posts/test.js
+++ blog/_posts/test.js
@@ -1,12 +1,1 @@
-var authorName="山边小溪";
-function doSomething(){
-    var blogName="梦想天空";
-    function innerSay(){
-        alert(blogName);
-    }
-    innerSay();
-}
-alert(authorName); //山边小溪
-alert(blogName); //脚本错误
-doSomething(); //梦想天空
-innerSay() //脚本错误
+alert(typeof memeda)
