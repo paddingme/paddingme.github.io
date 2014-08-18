a72e559a70ebde039eaf4aeb77c0b97233540ae5
paddingme <huhaibo2012@gmail.com>
2014-08-18 22:17:14 +0800

    comparison

--- demo/json-table/main.js
+++ demo/json-table/main.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     var url = "main.json";
-    //url是数据接口；json数据格式注意要用双引号
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function() {
@@ -12,41 +11,49 @@ window.onload = function() {
 }
 
 function updateTable(responseText) {
-    var template = document.getElementById('template');
-    var columnDates = JSON.parse(responseText);
+    var undisposed=document.getElementById('undisposed-data');
+    var disposed= document.getElementById('disposed-data');
+    var columnDatas = JSON.parse(responseText);
+    var tbody1 = document.createElement("tbody");
+    for (var i = 0; i < columnDatas.length; i++) {
+        tbody1.innerHTML+=  '<tr><td>' + columnDatas[i].id + "</td><td>" + columnDatas[i].classifyName + '</td><td>' + columnDatas[i].aa + '</td><td>' + columnDatas[i].bb + '</td></tr>';
+        undisposed.appendChild(tbody1);
+
+    }
+
     //遍历数组，如果数据column.classifyName 一样，则合并 一样的所有列。这里全部当做数字处理，如果有字符串自行处理。
-    var tbody = document.createElement("tbody");
-    for (var i = 0; i < columnDates.length; i++) {
+    var tbody2 = document.createElement("tbody");
+
+    for (var i = 0; i < columnDatas.length; i++) {
+
         var j = i + 1;
-        if (j < columnDates.length) {
-            while (columnDates[i].classifyName === columnDates[j].classifyName) {
-                columnDates[i].aa = parseInt(columnDates[i].aa) + parseInt(columnDates[j].aa);
-                columnDates[i].bb = parseInt(columnDates[i].bb) + parseInt(columnDates[j].bb);
+        if (j < columnDatas.length) {
+            while (columnDatas[i].classifyName === columnDatas[j].classifyName) {
+                columnDatas[i].aa = parseInt(columnDatas[i].aa) + parseInt(columnDatas[j].aa);
+                columnDatas[i].bb = parseInt(columnDatas[i].bb) + parseInt(columnDatas[j].bb);
                 j++;
-                if (j >= columnDates.length) break;
+                if (j >= columnDatas.length) break;
             }
             //打印出来第一个
-            tbody.innerHTML += '<tr><td>' + columnDates[i].id + "</td><td>" + columnDates[i].classifyName + '</td><td>' + columnDates[i].aa + '</td><td>' + columnDates[i].bb + '</td></tr>';
-            template.appendChild(tbody);
+            tbody2.innerHTML += '<tr><td>' + columnDatas[i].id + "</td><td>" + columnDatas[i].classifyName + '</td><td>' + columnDatas[i].aa + '</td><td>' + columnDatas[i].bb + '</td></tr>';
+            disposed.appendChild(tbody2);
             i = j - 1;
         }
     };
-
     //判断最后一个元素
-    //
-    if (columnDates.length >= 2) {
-        if (columnDates[(columnDates.length - 1)].classifyName !== columnDates[((columnDates.length - 2))].classifyName)
-            tbody.innerHTML += '<tr><td>' + columnDates[(columnDates.length - 1)].id + "</td><td>" + columnDates[(columnDates.length - 1)].classifyName + '</td><td>' + columnDates[(columnDates.length - 1)].aa + '</td><td>' + columnDates[(columnDates.length - 1)].bb + '</td></tr>';
-        template.appendChild(tbody);
+    if (columnDatas.length >= 2) {
+        if (columnDatas[(columnDatas.length - 1)].classifyName !== columnDatas[((columnDatas.length - 2))].classifyName)
+            tbody.innerHTML += '<tr><td>' + columnDatas[(columnDatas.length - 1)].id + "</td><td>" + columnDatas[(columnDatas.length - 1)].classifyName + '</td><td>' + columnDatas[(columnDatas.length - 1)].aa + '</td><td>' + columnDatas[(columnDatas.length - 1)].bb + '</td></tr>';
+        disposed.appendChild(tbody);
     }
 
     //打印合并后的数据是否对？！
-    // for (var i = 0; i < columnDates.length; i++) {
+    // for (var i = 0; i < columnDatas.length; i++) {
     //     console.log(
-    //         columnDates[i].id + "....." +
-    //         columnDates[i].classifyName + "....." +
-    //         columnDates[i].aa + "....." +
-    //         columnDates[i].bb + "....."
+    //         columnDatas[i].id + "....." +
+    //         columnDatas[i].classifyName + "....." +
+    //         columnDatas[i].aa + "....." +
+    //         columnDatas[i].bb + "....."
     //     )
     // };
 }
