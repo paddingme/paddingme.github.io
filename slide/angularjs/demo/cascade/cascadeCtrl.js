6de6f9d39225d8f09acfb822645b55a04f005bca
paddingme <huhaibo2012@gmail.com>
2015-07-09 10:31:45 +0800

    update

---
+++ slide/angularjs/demo/cascade/cascadeCtrl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('cascade').controller('cascadeCtrl', function ($scope, CityData) {
+  var vm = $scope.vm = {};
+  vm.countries = CityData;
+  // 更换国家的时候清空省
+  $scope.$watch('vm.country', function(country) {
+    vm.province = null;
+  });
+  // 更换省的时候清空城市
+  $scope.$watch('vm.province', function(province) {
+    vm.city = null;
+  });
+});
+
+
