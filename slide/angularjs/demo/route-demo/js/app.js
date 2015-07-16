0fe8cab1060be899c013070d38005e93825e632c
paddingme <huhaibo2012@gmail.com>
2015-07-16 18:05:42 +0800

    update angularjs slide

--- slide/angularjs/demo/route-demo/js/app.js
+++ slide/angularjs/demo/route-demo/js/app.js
@@ -3,22 +3,23 @@
 /* App Module */
 
 var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatControllers'
+    'ngRoute',
+    'phonecatControllers'
 ]);
 
 phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+    function($routeProvider) {
+        $routeProvider.
+        when('/phones', {
+            templateUrl: 'partials/phone-list.html',
+            controller: 'PhoneListCtrl'
+        }).
+        when('/phones/:phoneId', {
+            templateUrl: 'partials/phone-detail.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/phones'
+        });
+    }
+]);
